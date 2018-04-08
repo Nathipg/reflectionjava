07d5c91651ec12d76c0a8b86501216ad925a1c87
Nathalia Pissuti <nathi.pg2@gmail.com>
2018-04-08 11:04:16 -0300

    Getters 'n' Setters

--- Desafio1/src/classes/DesenhoAnimado.java
+++ Desafio1/src/classes/DesenhoAnimado.java
@@ -13,5 +13,70 @@ public class DesenhoAnimado {
     private int numeroTemporadas;
     private int totalEpisodios;
     private String sinopse;
+
+    //Getters 'n' Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getFaixaEtaria() {
+        return faixaEtaria;
+    }
+
+    public void setFaixaEtaria(String faixaEtaria) {
+        this.faixaEtaria = faixaEtaria;
+    }
+
+    public String getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public int getNumeroTemporadas() {
+        return numeroTemporadas;
+    }
+
+    public void setNumeroTemporadas(int numeroTemporadas) {
+        this.numeroTemporadas = numeroTemporadas;
+    }
+
+    public int getTotalEpisodios() {
+        return totalEpisodios;
+    }
+
+    public void setTotalEpisodios(int totalEpisodios) {
+        this.totalEpisodios = totalEpisodios;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
     
 }
