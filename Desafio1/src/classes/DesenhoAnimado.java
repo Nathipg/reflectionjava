27cc359c28227bb478ca98de409e6ddcd06cf5dd
Nathalia Pissuti <nathi.pg2@gmail.com>
2018-04-08 11:02:58 -0300

    Função principal criada. Alguns atributos de DesenhoAnimado definidos.

---
+++ Desafio1/src/classes/DesenhoAnimado.java
@@ -0,0 +1,17 @@
+package classes;
+
+/**
+ *
+ * @author nathipg e laionel lellis
+ */
+public class DesenhoAnimado {
+    private int id;
+    private String nome;
+    private String genero;
+    private String faixaEtaria;
+    private String anoLancamento;
+    private int numeroTemporadas;
+    private int totalEpisodios;
+    private String sinopse;
+    
+}
